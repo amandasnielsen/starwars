5aeb885b8b96abf7eb82ea04734464d94e603c4a
Jonathan Nielsen <jonathan.nielsen93@gmail.com>
2024-06-14 12:42:46 +0200

    Jonathan

--- jonathan/script.js
+++ jonathan/script.js
@@ -1,7 +1,10 @@
+// Setup all the element references
 const searchInput = document.querySelector('#search-input')
 const searchButton = document.querySelector('#search-button')
 const errorText = document.querySelector('#error-text')
 const result = document.querySelector('.result')
+
+// Setup all the character fields inside an object to make it easier to access them
 const characterFields = {
     name: document.querySelector('#character-name'),
     height: document.querySelector('#character-height'),
@@ -13,28 +16,35 @@ const characterFields = {
     gender: document.querySelector('#character-gender'),
 }
 
+// Add an event listener to the search button
 searchButton.addEventListener('click', getInfoFromApi)
 
+// Function to get the character information from the API
 async function getInfoFromApi() {
+    // Before we start the API request, we reset the character fields and form
+    // This is to make sure that the user gets a clean slate to search for a new character
     resetCharacterFieldsAndForm()
 
-    const characterId = searchInput.value.trim()
+    // Get the character search input value
+    const characterSearchInputValue = searchInput.value.trim()
 
-    if (!characterId) {
+    // Make sure that the user has entered a valid search term
+    if (!characterSearchInputValue) {
         errorText.innerText = 'Please enter a valid search term.'
         errorText.classList.remove('hidden')
 
         return
     }
 
-    const baseUrl = `https://swapi.dev/api/people/?search=${characterId}`
+    // Create the base URL for the API request
+    const baseUrl = `https://swapi.dev/api/people/?search=${characterSearchInputValue}`
 
-    console.log('Character ID:', characterId)
-    console.log('URL:', baseUrl)
-
+    // Add a try/catch block to handle any errors that might occur during the API request
     try {
+        // Send the API request
         const response = await fetch(baseUrl)
 
+        // Make sure that the API request was successful
         if (!response.ok) {
             console.error('Something went wrong with the API-request:', response.status)
 
@@ -44,9 +54,13 @@ async function getInfoFromApi() {
             return
         }
 
+        // Parse the response data
         const data = await response.json()
+
+        // Get the character from the response data
         const character = data.results[0]
         
+        // Make sure that the character was found
         if (data.count === 0 || !data) {
             errorText.innerText = 'Character was not found. Try Again.'
             errorText.classList.remove('hidden')
@@ -54,6 +68,7 @@ async function getInfoFromApi() {
             return
         }
 
+        // Set each character fields with the character information
         setCharacterFields(character)
 
     } catch (error) {
@@ -63,9 +78,11 @@ async function getInfoFromApi() {
         errorText.classList.remove('hidden')
     }
 
+    // Show the result section
     result.classList.remove('hidden')
 }
 
+// Function to set the character fields with the character information
 function setCharacterFields(character) {
     characterFields.name.innerHTML = character.name
     characterFields.height.innerHTML = character.height
@@ -77,6 +94,7 @@ function setCharacterFields(character) {
     characterFields.gender.innerHTML = character.gender
 }
 
+// Function to reset the character fields and form before a new search
 function resetCharacterFieldsAndForm() {
     result.classList.add('hidden')
     errorText.classList.add('hidden')
